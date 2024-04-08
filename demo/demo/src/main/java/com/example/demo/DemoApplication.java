536417386f979f6d922fcb4c2ec9296eb9e3cd4e
Sudarshan <sudarshangawande98@gmail.com>
2024-04-08 20:39:27 +0530

    Added demo application

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,14 @@
-package com.example.OpenShiftDemo;
+package com.example.demo;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenShiftDemoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OpenShiftDemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
